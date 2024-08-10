424473e5ba650ca0d4ffbc0df5270c6b13a431ab
Priyanshu Raghav <priyanshuraghav202@gmail.com>
2024-08-10 15:24:21 +0530

    Add Project

---
+++ src/main/java/com/scm/SmartContactsManager/SmartContactsManagerApplication.java
@@ -0,0 +1,13 @@
+package com.scm.SmartContactsManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartContactsManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartContactsManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scm/SmartContactsManager/SmartContactsManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scm.SmartContactsManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartContactsManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
